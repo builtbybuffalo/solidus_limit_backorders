370c9a7e85c4bdb94a17824f3dc5bc551a5ed68d
njakic <nikola@arsfutura.co>
2015-08-25 16:35:35 +0200

    Port from spree to solidus

---
+++ lib/spree_limit_backorders.rb
@@ -0,0 +1,5 @@
+require 'spree_core'
+require 'spree_limit_backorders/engine'
+require 'spree_limit_backorders/extensions'
+require 'spree_limit_backorders/extensions/models/quantifier'
+require 'spree_limit_backorders/extensions/controllers/admin/stock_items_controller'
---
+++ lib/spree_limit_backorders/extensions.rb
@@ -0,0 +1,11 @@
+module SpreeLimitBackorders
+  module Extensions
+    module Models
+    end
+
+    module Controllers
+      module Admin
+      end
+    end
+  end
+end
